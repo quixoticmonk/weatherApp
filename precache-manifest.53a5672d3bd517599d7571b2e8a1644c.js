a141ee77f243910db98496de41c13b0855b6a61c
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2020-01-11 14:03:30 -0500

    Updates

---
+++ precache-manifest.53a5672d3bd517599d7571b2e8a1644c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010bdeeed09a338c6853aba448f20ce2",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "6736f8dfec63d7d837af",
+    "url": "/weatherApp/static/css/2.6f8bcf7b.chunk.css"
+  },
+  {
+    "revision": "79e285138d2ade11a97f",
+    "url": "/weatherApp/static/css/main.16daef2f.chunk.css"
+  },
+  {
+    "revision": "6736f8dfec63d7d837af",
+    "url": "/weatherApp/static/js/2.f4447ce3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weatherApp/static/js/2.f4447ce3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79e285138d2ade11a97f",
+    "url": "/weatherApp/static/js/main.fe8b084f.chunk.js"
+  },
+  {
+    "revision": "552c4cf252a8a8f1a25c",
+    "url": "/weatherApp/static/js/runtime-main.b1b8ee91.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weatherApp/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weatherApp/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weatherApp/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weatherApp/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weatherApp/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weatherApp/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
