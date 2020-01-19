e8611541cd902d2e497a5f2e7173ad1ac609c088
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2020-01-19 11:15:13 -0500

    Updated api url to https

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
     e.preventDefault()
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=${UNITS}`)
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=${UNITS}`)
     const data = await api_call.json()
     
     if(city && country){
